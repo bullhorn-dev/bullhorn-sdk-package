44e08e5de4924d5001b0e9abd9250a0ad6dbaedb
Peter Dolganov <pdolganov@freeconferencecall.com>
2026-03-05 13:07:47 +0100

    Refactor links resolver

--- Sources/BullhornSdk/Classes/BHLinkResolver.swift
+++ Sources/BullhornSdk/Classes/BHLinkResolver.swift
@@ -11,59 +11,62 @@ enum UniversalLinkType: String {
     case unknown
 }
 
-// MARK: - UniversalNotificationInfo
+// MARK: - UniversalLinkInfo
 
-struct UniversalNotificationInfo {
+struct UniversalLinkInfo {
         
     let type: UniversalLinkType
-    let username: String
+    let username: String?
     let alias: String?
+    
+    static let unknown = UniversalLinkInfo(type: .unknown, username: nil, alias: nil)
 }
 
 // MARK: - BHLinkResolver
 
 class BHLinkResolver {
 
-    enum LinkResolverResult {
-        case success(p: [String : Any])
-        case failure(e: String)
-    }
-
-    typealias LinkResolverCompletion = (LinkResolverResult?) -> Void
-
     static let shared = BHLinkResolver()
     
-    fileprivate let validShortURLPathFirstComponents: Set<String> = ["posts", "categories"]
+    fileprivate enum FirstPathComponents: String {
+        case podcasts
+        case posts
+        case categories
+    }
     
-    fileprivate let podcastLinkDelimitersCount = 1
+    fileprivate let podcastLinkDelimitersCount = 2
     fileprivate let categoryLinkDelimitersCount = 2
-    fileprivate let episodeLinkDelimitersCount = 3
+    fileprivate let episodeLinkDelimitersCount = 4
 
-    func validateLink(_ url: URL) -> UniversalLinkType {
+    func validateLink(_ url: URL) -> UniversalLinkInfo {
 
         guard let webSiteURL1 = URL.init(string: BHAppConfiguration.shared.webSiteURL1String) else { return .unknown }
         guard let webSiteURL2 = URL.init(string: BHAppConfiguration.shared.webSiteURL2String) else { return .unknown }
         if url.host != webSiteURL2.host && url.host != webSiteURL1.host { return .unknown }
 
-        let pathComponentsWithoutDelimiters = url.pathComponentsWithoutDelimiters
+        var pathComponentsWithoutDelimiters = url.pathComponentsWithoutDelimiters
+        
+        if let firstComponent = pathComponentsWithoutDelimiters.first, firstComponent == "content" {
+            pathComponentsWithoutDelimiters.removeFirst()
+        } else {
+            return .unknown
+        }
 
-        /// podcast link
-        if pathComponentsWithoutDelimiters.count == podcastLinkDelimitersCount {
-            return .podcast
-        }
-        
-        /// episode link
-        if pathComponentsWithoutDelimiters.count == categoryLinkDelimitersCount {
-            let firstPathComponent = pathComponentsWithoutDelimiters[0]
-            guard validShortURLPathFirstComponents.contains(firstPathComponent) else { return .unknown }
-            return .category
-        }
-        
-        /// episode link
-        if pathComponentsWithoutDelimiters.count == episodeLinkDelimitersCount {
-            let secondPathComponent = pathComponentsWithoutDelimiters[1]
-            guard validShortURLPathFirstComponents.contains(secondPathComponent) else { return .unknown }
-            return .episode
+        if let firstComponent = pathComponentsWithoutDelimiters.first,
+           firstComponent == FirstPathComponents.podcasts.rawValue {
+            if pathComponentsWithoutDelimiters.count == podcastLinkDelimitersCount {
+                let username = pathComponentsWithoutDelimiters[podcastLinkDelimitersCount-1]
+                return UniversalLinkInfo(type: .podcast, username: username, alias: nil)
+            } else if pathComponentsWithoutDelimiters.count == episodeLinkDelimitersCount {
+                let username = pathComponentsWithoutDelimiters[episodeLinkDelimitersCount-3]
+                let alias = pathComponentsWithoutDelimiters[episodeLinkDelimitersCount-1]
+                return UniversalLinkInfo(type: .episode, username: username, alias: alias)
+            }
+        } else if let firstPathComponent = pathComponentsWithoutDelimiters.first,
+                    firstPathComponent == FirstPathComponents.categories.rawValue,
+                    pathComponentsWithoutDelimiters.count == categoryLinkDelimitersCount {
+            let alias = pathComponentsWithoutDelimiters[categoryLinkDelimitersCount-1]
+            return UniversalLinkInfo(type: .category, username: nil, alias: alias)
         }
 
         return .unknown
@@ -71,15 +74,15 @@ class BHLinkResolver {
 
     func resolveUniversalLink(_ url: URL) -> Bool {
 
-        let result = validateLink(url)
+        let info = validateLink(url)
 
-        switch result {
+        switch info.type {
         case .podcast:
-            return resolvePodcastLink(url)
+            return resolvePodcastLink(info)
         case .episode:
-            return resolveEpisodeLink(url)
+            return resolveEpisodeLink(info)
         case .category:
-            return resolveCategoryLink(url)
+            return resolveCategoryLink(info)
         case .unknown:
             BHLog.w("Unsupported type of universal link")
         }
@@ -89,66 +92,106 @@ class BHLinkResolver {
 
     // MARK: - Private
 
-    fileprivate func resolvePodcastLink(_ url: URL) -> Bool {
+    fileprivate func resolvePodcastLink(_ linkInfo: UniversalLinkInfo) -> Bool {
+        BHLog.p("\(#function) - username: \(linkInfo.username ?? "nil")")
+        
         var result = false
-        let pathComponentsWithoutDelimiters = url.pathComponentsWithoutDelimiters
 
-        if pathComponentsWithoutDelimiters.count == podcastLinkDelimitersCount {
-            let username = pathComponentsWithoutDelimiters[0]
-            
-            if !username.isEmpty {
-                if BHReachabilityManager.shared.isConnected() {
-                    BHUserManager.shared.getUserByUsername(username) { result in
-                        switch result {
-                        case .success(user: let user):
-                            DispatchQueue.main.async {
-                                let bundle = Bundle.module
-                                let storyboard = UIStoryboard(name: StoryboardName.main, bundle: bundle)
-                                let vc = storyboard.instantiateViewController(withIdentifier: BHUserDetailsViewController.storyboardIndentifer) as! BHUserDetailsViewController
-                                vc.user = user
+        if let validUsername = linkInfo.username {
+            if BHReachabilityManager.shared.isConnected() {
+                BHUserManager.shared.getUserByUsername(validUsername) { result in
+                    switch result {
+                    case .success(user: let user):
+                        DispatchQueue.main.async {
+                            let bundle = Bundle.module
+                            let storyboard = UIStoryboard(name: StoryboardName.main, bundle: bundle)
+                            let vc = storyboard.instantiateViewController(withIdentifier: BHUserDetailsViewController.storyboardIndentifer) as! BHUserDetailsViewController
+                            vc.user = user
                                 
-                                UIApplication.topNavigationController()?.pushViewController(vc, animated: true)
-                            }
-                            
-                        case .failure(error: let e):
-                            BHLog.w(e)
-                            break
+                            UIApplication.topNavigationController()?.pushViewController(vc, animated: true)
                         }
+                            
+                    case .failure(error: let e):
+                        BHLog.w(e)
+                        break
                     }
-                } else {
-                    UIApplication.topViewController()?.showError("Failed to load episode details. The Internet connection is lost.")
                 }
-                result = true
+            } else {
+                UIApplication.topViewController()?.showError("Failed to load episode details. The Internet connection is lost.")
             }
-        } else {
-            BHLog.w("Failed to read username from universal link")
+            result = true
         }
         
         return result
     }
 
-    fileprivate func resolveEpisodeLink(_ url: URL) -> Bool {
+    fileprivate func resolveEpisodeLink(_ linkInfo: UniversalLinkInfo) -> Bool {
+        BHLog.p("\(#function) - username: \(linkInfo.username ?? "nil"), alias: \(linkInfo.alias ?? "nil")")
+
         var result = false
-        let pathComponentsWithoutDelimiters = url.pathComponentsWithoutDelimiters
 
-        if pathComponentsWithoutDelimiters.count == episodeLinkDelimitersCount {
-            let username = pathComponentsWithoutDelimiters[0]
-            let alias = pathComponentsWithoutDelimiters[2]
-
-            if !username.isEmpty && !alias.isEmpty {
-                if BHReachabilityManager.shared.isConnected() {
-                    BHPostsManager.shared.getPostByAlias(username, postAlias: alias) { result in
-                        switch result {
-                        case .success(post: let post):
-                            DispatchQueue.main.async {
-                                let bundle = Bundle.module
-                                let storyboard = UIStoryboard(name: StoryboardName.main, bundle: bundle)
-                                let vc = storyboard.instantiateViewController(withIdentifier: BHPostDetailsViewController.storyboardIndentifer) as! BHPostDetailsViewController
-                                vc.post = post
+        if let validUsername = linkInfo.username, let validAlias = linkInfo.alias {
+            if BHReachabilityManager.shared.isConnected() {
+                BHPostsManager.shared.getPostByAlias(validUsername, postAlias: validAlias) { result in
+                    switch result {
+                    case .success(post: let post):
+                        DispatchQueue.main.async {
+                            let bundle = Bundle.module
+                            let storyboard = UIStoryboard(name: StoryboardName.main, bundle: bundle)
+                            let vc = storyboard.instantiateViewController(withIdentifier: BHPostDetailsViewController.storyboardIndentifer) as! BHPostDetailsViewController
+                            vc.post = post
                                 
-                                UIApplication.topNavigationController()?.pushViewController(vc, animated: true)
-                            }
+                            UIApplication.topNavigationController()?.pushViewController(vc, animated: true)
+                        }
                             
+                    case .failure(error: let e):
+                        BHLog.w(e)
+                        DispatchQueue.main.async {
+                            UIApplication.topViewController()?.showError("Failed to load episode details. This episode is no longer available.")
+                        }
+                        break
+                    }
+                }
+            } else {
+                UIApplication.topViewController()?.showError("Failed to load episode details. The Internet connection is lost.")
+            }
+            result = true
+        }
+
+        return result
+    }
+    
+    fileprivate func resolveCategoryLink(_ linkInfo: UniversalLinkInfo) -> Bool {
+        BHLog.p("\(#function) - alias: \(linkInfo.alias ?? "nil")")
+
+        var result = false
+
+        if let validAlias = linkInfo.alias {
+            BHNetworkManager.shared.splitUsersForCarPlay()
+            if let categoryModel = BHNetworkManager.shared.getCategoryModel(with: validAlias) {
+                let bundle = Bundle.module
+                let storyboard = UIStoryboard(name: StoryboardName.main, bundle: bundle)
+                let vc = storyboard.instantiateViewController(withIdentifier: BHCategoryViewController.storyboardIndentifer) as! BHCategoryViewController
+                vc.categoryModel = categoryModel
+                    
+                UIApplication.topNavigationController()?.pushViewController(vc, animated: true)
+            } else {
+                if BHReachabilityManager.shared.isConnected() {
+                    let networkId = BHAppConfiguration.shared.networkId
+                    BHNetworkManager.shared.fetchCategories(networkId) { result in
+                        switch result {
+                        case .success:
+                            DispatchQueue.main.async {
+                                BHNetworkManager.shared.splitUsersForCarPlay()
+                                if let categoryModel = BHNetworkManager.shared.getCategoryModel(with: validAlias) {
+                                    let bundle = Bundle.module
+                                    let storyboard = UIStoryboard(name: StoryboardName.main, bundle: bundle)
+                                    let vc = storyboard.instantiateViewController(withIdentifier: BHCategoryViewController.storyboardIndentifer) as! BHCategoryViewController
+                                    vc.categoryModel = categoryModel
+                                        
+                                    UIApplication.topNavigationController()?.pushViewController(vc, animated: true)
+                                }
+                            }
                         case .failure(error: let e):
                             BHLog.w(e)
                             DispatchQueue.main.async {
@@ -158,66 +201,10 @@ class BHLinkResolver {
                         }
                     }
                 } else {
-                    UIApplication.topViewController()?.showError("Failed to load episode details. The Internet connection is lost.")
+                    UIApplication.topViewController()?.showError("Failed to load category details. The Internet connection is lost.")
                 }
-                result = true
             }
-        } else {
-            BHLog.w("Failed to read username and alias from universal link")
-        }
-
-        return result
-    }
-    
-    fileprivate func resolveCategoryLink(_ url: URL) -> Bool {
-        var result = false
-        let pathComponentsWithoutDelimiters = url.pathComponentsWithoutDelimiters
-
-        if pathComponentsWithoutDelimiters.count == categoryLinkDelimitersCount {
-            let alias = pathComponentsWithoutDelimiters[1]
-            
-            if !alias.isEmpty {
-                BHNetworkManager.shared.splitUsersForCarPlay()
-                if let categoryModel = BHNetworkManager.shared.getCategoryModel(with: alias) {
-                    let bundle = Bundle.module
-                    let storyboard = UIStoryboard(name: StoryboardName.main, bundle: bundle)
-                    let vc = storyboard.instantiateViewController(withIdentifier: BHCategoryViewController.storyboardIndentifer) as! BHCategoryViewController
-                    vc.categoryModel = categoryModel
-                    
-                    UIApplication.topNavigationController()?.pushViewController(vc, animated: true)
-                } else {
-                    if BHReachabilityManager.shared.isConnected() {
-                        let networkId = BHAppConfiguration.shared.networkId
-                        BHNetworkManager.shared.fetchCategories(networkId) { result in
-                            switch result {
-                            case .success:
-                                DispatchQueue.main.async {
-                                    BHNetworkManager.shared.splitUsersForCarPlay()
-                                    if let categoryModel = BHNetworkManager.shared.getCategoryModel(with: alias) {
-                                        let bundle = Bundle.module
-                                        let storyboard = UIStoryboard(name: StoryboardName.main, bundle: bundle)
-                                        let vc = storyboard.instantiateViewController(withIdentifier: BHCategoryViewController.storyboardIndentifer) as! BHCategoryViewController
-                                        vc.categoryModel = categoryModel
-                                        
-                                        UIApplication.topNavigationController()?.pushViewController(vc, animated: true)
-                                    }
-                                }
-                            case .failure(error: let e):
-                                BHLog.w(e)
-                                DispatchQueue.main.async {
-                                    UIApplication.topViewController()?.showError("Failed to load episode details. This episode is no longer available.")
-                                }
-                                break
-                            }
-                        }
-                    } else {
-                        UIApplication.topViewController()?.showError("Failed to load category details. The Internet connection is lost.")
-                    }
-                }
-                result = true
-            }
-        } else {
-            BHLog.w("Failed to read username from universal link")
+            result = true
         }
         
         return result
