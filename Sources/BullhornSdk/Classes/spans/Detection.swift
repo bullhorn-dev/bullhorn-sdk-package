116994393daab21497e6f750c4c5c8c380507d0b
Peter Dolganov <pdolganov@freeconferencecall.com>
2026-05-11 13:19:11 +0200

    RichTextView and RichLabel with html tags support

---
+++ Sources/BullhornSdk/Classes/spans/Detection.swift
@@ -0,0 +1,47 @@
+
+import Foundation
+
+public struct DetectionContext {
+
+    public let range: Range<String.Index>
+    public let text: String
+    public let existingAttributes: [AttributesProvider]
+
+    public func firstExistingAttributeValue(for key: NSAttributedString.Key) -> Any? {
+        for a in existingAttributes {
+            if let value = a.attributes[key] {
+                return value
+            }
+        }
+        return nil
+    }
+}
+
+public protocol DetectionTuning {
+    func style(context: DetectionContext) -> AttributesProvider
+}
+
+public struct DetectionTuner: DetectionTuning {
+
+    public func style(context: DetectionContext) -> AttributesProvider {
+        return _style(context)
+    }
+
+    private let _style: (DetectionContext) -> AttributesProvider
+
+    public init(style: @escaping (DetectionContext) -> AttributesProvider) {
+        _style = style
+    }
+}
+
+extension Attrs: DetectionTuning {
+    public func style(context _: DetectionContext) -> AttributesProvider {
+        return self
+    }
+}
+
+extension Dictionary: DetectionTuning where Key == NSAttributedString.Key, Value == Any {
+    public func style(context _: DetectionContext) -> AttributesProvider {
+        return self
+    }
+}
--- Sources/BullhornSdk/Classes/views/cells/BHPostDescriptionCell.swift
+++ Sources/BullhornSdk/Classes/views/cells/BHPostDescriptionCell.swift
@@ -6,7 +6,7 @@ class BHPostDescriptionCell: UITableViewCell {
     
     class var reusableIndentifer: String { return String(describing: self) }
     
-    @IBOutlet weak var textView: BHRichTextView!
+    @IBOutlet weak var textView: RichTextView!
     
     var post: BHPost? {
         didSet {
@@ -20,37 +20,75 @@ class BHPostDescriptionCell: UITableViewCell {
     }
 
     // MARK: - Private
+    
+    func stringWithAttributes() -> NSAttributedString {
+        guard let validText = post?.description else { return NSAttributedString() }
+        
+        let font = UIFont.fontWithName(.robotoRegular, size: 14)
+        let base = Attrs().font(font).foregroundColor(.primary())
+        let links = Attrs().font(font).foregroundColor(.accent())
+        let timestamps = Attrs().font(font).foregroundColor(.accent())
+        let a = Attrs().font(font).foregroundColor(.primary())
+        let u = Attrs().underlineStyle(.single)
+        let i = TagTuner { info in
+            var set = Set<String>()
+            set.insert(info.tag.name)
+            info.outerTags.forEach { set.insert($0.name) }
 
-    fileprivate func setup() {
-        guard let validText = post?.description else { return }
-
-        textView.textColor = .primary()
-        textView.font = .fontWithName(.robotoRegular, size: 14)
-        textView.textAlignment = .left
-
-        textView.setText(validText)
-
-        textView.onLinkTap = { url in
-            BHLog.p("Open link tag: \(url.absoluteString)")
-
-            UIApplication.topViewController()?.presentSafari(url)
+            let attrs = Attrs()
+            if set.contains("b") && set.contains("i") {
+                attrs.font(UIFont(name: "HelveticaNeue-BoldItalic", size: 14)!)
+            } else if set.contains("i") {
+                attrs.font(UIFont(name: "HelveticaNeue-Italic", size: 14)!)
+            } else if set.contains("b") {
+                attrs.font(UIFont(name: "HelveticaNeue-Bold", size: 14)!)
+            }
+            return attrs
         }
 
-        textView.onTimestampTap = { seconds in
-            BHLog.p("Open timestamp tag seconds: \(seconds)")
-            
-            guard let validPost = self.post else { return }
-            
-            if validPost.isLiveNow() {
-                BHLivePlayer.shared.playRequest(with: validPost)
-            } else {
-                if BHHybridPlayer.shared.isPostActive(validPost.id) {
-                    BHHybridPlayer.shared.seek(to: Double(seconds), resume: true)
+        let attributedText = validText
+            .style(tags: ["a": a, "u": u, "i": i])
+            .styleBase(base)
+            .styleLinks(links)
+            .styleTimestamps(timestamps)
+            .attributedString
+
+        return attributedText
+    }
+
+    fileprivate func setup() {
+        textView.backgroundColor = .clear
+        textView.textAlignment = .left
+        textView.isEnabled = true
+        textView.highlightedLinkAttributes = Attrs().foregroundColor(.secondary()).attributes
+        textView.highlightedTimestampAttributes = Attrs().foregroundColor(.secondary()).attributes
+        textView.onLinkTouchUpInside = { _, val in
+            if let linkStr = val as? String, let url = URL(string: linkStr) {
+                BHLog.p("Open link: \(url.absoluteString)")
+
+                UIApplication.topViewController()?.presentSafari(url)
+            }
+        }
+        textView.onTimestampTouchUpInside = { _, val in
+            if let timestamp = val as? String {
+                let seconds = timestamp.secondFromString
+
+                BHLog.p("Open timestamp: \(timestamp)")
+
+                guard let validPost = self.post else { return }
+                
+                if validPost.isLiveNow() {
+                    BHLivePlayer.shared.playRequest(with: validPost)
                 } else {
-                    BHHybridPlayer.shared.playRequest(with: validPost, playlist: [], autoplayContext: .podcast, position: Double(seconds))
+                    if BHHybridPlayer.shared.isPostActive(validPost.id) {
+                        BHHybridPlayer.shared.seek(to: Double(seconds), resume: true)
+                    } else {
+                        BHHybridPlayer.shared.playRequest(with: validPost, playlist: [], autoplayContext: .podcast, position: Double(seconds))
+                    }
                 }
             }
         }
+        textView.attributedText = stringWithAttributes()
         
         /// accessibility
         self.isAccessibilityElement = true
